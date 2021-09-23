e13a64269cfbcc02f8d8876c4ec088d0ae8331ae
phamduchung2k <72064509+phamduchung2k@users.noreply.github.com>
2021-09-23 12:23:09 +0700

    Add files via upload

---
+++ TestThreadPool.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestThreadPool {
+    public static void main(String[] args) {
+        // tạo một pool chứa 5 threads
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread("" + i);
+            // call phương thức execute của ExecutorService
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+ 
+        System.out.println("Finished all threads");
+    }
+}
